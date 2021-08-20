1a0bdfd57f415cf673b9303df2308414e9fafa7a
Sammy Shear <dspfgh34@gmail.com>
2021-08-19 20:13:50 -0400

    Add files via upload

---
+++ precache-manifest.36c9466cac1ede2ce2d20ecadf11fc17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578b5880ef809adc15bb",
+    "url": "/vue3-calc/css/app.0ba985b6.css"
+  },
+  {
+    "revision": "f10c51eb092ca2051f316257ed3f9851",
+    "url": "/vue3-calc/index.html"
+  },
+  {
+    "revision": "578b5880ef809adc15bb",
+    "url": "/vue3-calc/js/app.0d15d59f.js"
+  },
+  {
+    "revision": "1302cc2ec3b595c256a5",
+    "url": "/vue3-calc/js/chunk-vendors.bd8c00f3.js"
+  },
+  {
+    "revision": "ecf0587abed8464b1851900acf86b327",
+    "url": "/vue3-calc/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue3-calc/robots.txt"
+  }
+]);
